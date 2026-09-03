a9de3b6d394b959a7f36881c9b57c87be2f820c1
Ksenia P <morimoeee@gmail.com>
2026-09-03 14:08:51 +0300

    chore: настроить подключение к базе и DI для Account

--- MiniBank.API/Program.cs
+++ MiniBank.API/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MiniBank.BusinessLogic.Ports;
+using MiniBank.BusinessLogic.Services;
+using MiniBank.DataAccess.Adapters;
+using MiniBank.DataAccess.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MiniBankDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountService, AccountService>();
 
 var app = builder.Build();
 
