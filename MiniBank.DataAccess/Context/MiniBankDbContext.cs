8997bb919579e8dff8ec3eb525255e8856e5e9a8
Ksenia P <morimoeee@gmail.com>
2026-09-08 15:07:06 +0300

    chore: добавить миграции EF Core для второго счёта и хеша пароля

--- MiniBank.DataAccess/Context/MiniBankDbContext.cs
+++ MiniBank.DataAccess/Context/MiniBankDbContext.cs
@@ -13,11 +13,12 @@ namespace MiniBank.DataAccess.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, Name = "Test User", Email = "test@minibank.com", PasswordHash = "..." }
+                new User { Id = 1, Name = "Test User", Email = "test@minibank.com", PasswordHash = "$2a$11$U62BKHFnsQZutN5WVy3XaukmPrGzOS4Xc.yudqZlU0XV9f19JYK92" }
             );
 
             modelBuilder.Entity<Account>().HasData(
-                new Account { Id = 1, UserId = 1, AccountNumber = "MB-000001", Currency = "MDL", Balance = 1000m }
+                new Account { Id = 1, UserId = 1, AccountNumber = "MB-000001", Currency = "MDL", Balance = 1000m },
+                new Account { Id = 2, UserId = 1, AccountNumber = "MB-000002", Currency = "MDL", Balance = 500m }
             );
         }
     }
---
+++ MiniBank.DataAccess/Migrations/20260908102558_SeedRealPasswordHash.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniBank.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRealPasswordHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$U62BKHFnsQZutN5WVy3XaukmPrGzOS4Xc.yudqZlU0XV9f19JYK92");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "...");
+        }
+    }
+}
