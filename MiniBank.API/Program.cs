e0ba8b127e92c59e0d8366f4dd0bce976d34fc77
Ksenia P <morimoeee@gmail.com>
2026-09-08 15:06:49 +0300

    chore: настроить JWT authentication middleware

--- MiniBank.API/Program.cs
+++ MiniBank.API/Program.cs
@@ -1,31 +1,37 @@
-using Microsoft.EntityFrameworkCore;
-using MiniBank.BusinessLogic.Ports;
-using MiniBank.BusinessLogic.Services;
-using MiniBank.DataAccess.Adapters;
-using MiniBank.DataAccess.Context;
+    using Microsoft.EntityFrameworkCore;
+    using MiniBank.BusinessLogic.Ports;
+    using MiniBank.BusinessLogic.Services;
+    using MiniBank.BusinessLogic.Settings;
+    using MiniBank.DataAccess.Adapters;
+    using MiniBank.DataAccess.Context;
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<MiniBankDbContext>(options =>
-options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<IAccountRepository, AccountRepository>();
-builder.Services.AddScoped<IAccountService, AccountService>();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<MiniBankDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+    builder.Services.AddScoped<IAccountService, AccountService>();
+    builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    var jwtSettings = builder.Configuration.GetSection("Jwt").Get<JwtSettings>();
+    builder.Services.AddSingleton(jwtSettings);
 
-var app = builder.Build();
+    var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
-app.UseAuthorization();
+    app.UseAuthorization();
 
-app.MapControllers();
+    app.MapControllers();
 
-app.Run();
+    app.Run();
