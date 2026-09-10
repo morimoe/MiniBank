287ecc02f734c807f646f822e2f5c40f2eaa030f
Ksenia P <morimoeee@gmail.com>
2026-09-10 15:48:49 +0300

    fix: изменить email первого тестового пользователя

--- MiniBank.DataAccess/Context/MiniBankDbContext.cs
+++ MiniBank.DataAccess/Context/MiniBankDbContext.cs
@@ -13,7 +13,7 @@ namespace MiniBank.DataAccess.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, Name = "Test User1", Email = "test1", PasswordHash = "$2a$11$U62BKHFnsQZutN5WVy3XaukmPrGzOS4Xc.yudqZlU0XV9f19JYK92" },
+                new User { Id = 1, Name = "Test User1", Email = "test1@minibank.com", PasswordHash = "$2a$11$U62BKHFnsQZutN5WVy3XaukmPrGzOS4Xc.yudqZlU0XV9f19JYK92" },
                 new User { Id = 2, Name = "Test User2", Email = "test2", PasswordHash = "$2a$11$Svw55ZLBpVfb29IOdOCKLeiLRAbbXcW0WgDT/4E21HPc0hS2e2y/K" }
             );
 
---
+++ MiniBank.DataAccess/Migrations/20260910121937_ChangeEmailFirstUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniBank.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeEmailFirstUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Email",
+                value: "test1@minibank.com");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Email",
+                value: "test1");
+        }
+    }
+}
