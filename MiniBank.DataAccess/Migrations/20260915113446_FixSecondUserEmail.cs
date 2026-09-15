0ada9add05a71acbb2bb2533a298227769a22f7b
Ksenia P <morimoeee@gmail.com>
2026-09-15 14:40:02 +0300

    chore: исправить email тестового пользователя в сидинге

---
+++ MiniBank.DataAccess/Migrations/20260915113446_FixSecondUserEmail.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniBank.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSecondUserEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Email",
+                value: "test2@minibank.com");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Email",
+                value: "test2");
+        }
+    }
+}
