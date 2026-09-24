f35b252ebcb730bd49fb56b31502298d78227e98
Ksenia P <morimoeee@gmail.com>
2026-09-24 12:18:44 +0300

    chore: убрать неиспользуемую Bearer-авторизацию из Swagger

--- MiniBank.API/Program.cs
+++ MiniBank.API/Program.cs
@@ -1,47 +1,18 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using MiniBank.BusinessLogic.Ports;
 using MiniBank.BusinessLogic.Services;
 using MiniBank.BusinessLogic.Settings;
 using MiniBank.DataAccess.Adapters;
 using MiniBank.DataAccess.Context;
 using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "¬ведите токен"
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<MiniBankDbContext>(options =>
 options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
